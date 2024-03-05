3666c5754a8a1ff3436f30b8f63be3484fc27503
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-03-05 20:05:16 -0300

    Upload - move files desktop

---
+++ Aula - PPW - Condicionais/estruturalIf3.js
@@ -0,0 +1,10 @@
+const nota = 9.1
+
+if (nota >= 9) {
+    console.log('Quadro de honra!');
+    console.log('parabéns');
+}  else {
+    console.log( + nota + ", sua nota foi abaixo do exigido!");
+}
+console.log('Fim');
+
