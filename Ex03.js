9f95c803e20635777e2a93f30eb489368e88e523
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-03-11 17:47:14 -0300

    Revert "Atv - pesq/exs"

---
+++ Ex03.js
@@ -0,0 +1,14 @@
+
+function exibirNum (n1, n2, n3) {
+   
+    var n1 = getElementById('n1').value;
+    var n2 = getElementById('n2').value;
+    var n3 = getElementById('n3').value;
+    
+    console.log("Os valores são:" + n1 + "," + n2 + "e" +n3)
+   
+  }
+
+
+
+
