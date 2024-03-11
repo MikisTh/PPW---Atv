704cda3e685d3f50aea8a000fb61f4261dd2c2ca
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-03-11 15:00:38 -0300

    Update Atv02Question03.js

--- Atv02Question03.js
+++ Atv02Question03.js
@@ -1,10 +1,13 @@
 function maioridade (int idade) {
-  idade=18
-  return idade;
+    idade=18
+       
+        let idade = prompt ("Informe sua idade:");
+    
+    if(idade>=18){
+    console.log("Você é maior de idade:");
+    }else{
+    console.log("Você é menor de idade:");
+    }
+}
+return (idade);
 
-let idade = prompt ("Informe sua idade:");
-
-if(idade>=18){
-  console.log("Você é maior de idade:");
-}else{
-  console.log("Você é menor de idade:");
