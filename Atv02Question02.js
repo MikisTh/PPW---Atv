59b5ec0fba2ffe7593a4e92f3d61e2eaa271ab9f
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-03-11 14:00:46 -0300

    Create Atv02Question02.js

---
+++ Atv02Question02.js
@@ -0,0 +1,12 @@
+function adinhaNum (int n, n1){
+  n=n1;
+  return
+}
+
+let n1 = prompt("Informe o valor do número pensado:");
+
+for(n1=1;n1<=100; n1++){
+  for (n=1; n<=100; n++){
+    console.log("n1=n");
+
+  
