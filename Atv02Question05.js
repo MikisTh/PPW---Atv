39d9de18853d457f7e920271243c42a2a1d826cf
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-03-11 14:16:55 -0300

    Create Atv02Question05.js

---
+++ Atv02Question05.js
@@ -0,0 +1,10 @@
+function saqueLimite (int valor){
+  i = valor;
+return(i);
+  
+let valor = prompt ("Informe o valor que deseja sacar:");
+
+if(i%10===0){
+  console.log("Saque realizado com sucesso!");
+}else{
+  console.log("Valor inválido, digite um valor múltiplo de 10!");
