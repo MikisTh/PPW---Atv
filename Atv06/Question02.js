90b8ab63cbd93ca7b44e67103110664449140caf
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-02 12:03:54 -0300

    Update Question02.js
    Add

--- Atv06/Question02.js
+++ Atv06/Question02.js
@@ -2,11 +2,17 @@
 
 function AutenticantionUser () {
 
-let nome;
-prompt("Informe o nome:");
+let nome = prompt("Informe o nome:");
 console.log(nome);
 
-let senha;
-prompt("Informe a senha:");
+let senha = prompt("Informe a senha:");
 console.log(senha);
+
+while( nome === senha){
+    alert ("nome e senha iguais, por favor reescreva ambos diferentes!")
+    nome = prompt("recreva o nome novamente")
+    senha = prompt("escreva a senha novamente")
+    
+    }alert("cadastro realizado!")
+
 }
