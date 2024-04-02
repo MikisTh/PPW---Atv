60e57f2992aa5ee5eee5cb5d316fcf3130954676
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-02 12:03:19 -0300

    Update Question01.js
    Add

--- Atv06/Question01.js
+++ Atv06/Question01.js
@@ -1,10 +1,14 @@
 //Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 
 var nota;
-prompt("Insira uma nota de 0 a 10");
+var nota = parseInt(prompt("Insira uma nota entre 0 (Zero) e 10 (Dez)"));
 
-if( nota <= 0) {
-    console.log("Valor Inválido")
-} else if (nota > 10) {
-    console.log("Valor Inválido")
-}
+if(( nota <= 0)&&(nota>=10)) {
+    console.log("Valor Inválido");
+} while((n<0)||(n>10)){
+    var nota = parseInt(prompt("Insira uma nota entre 0 (Zero) e 10 (Dez)"));
+        if ((n<0)||(n>10))
+            {console.log("Valor inválido")
+        }else {}}
+    console.log("Valor válido")
+console.log("Fim")
