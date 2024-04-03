114ce0ff51270fa721a8b858eb518dac75e9c6cf
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-03 10:44:18 -0300

    Update Question01.js
    Add

--- Atv07/Question01.js
+++ Atv07/Question01.js
@@ -1,12 +1,14 @@
 //Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
 
-function potencia () {
-    let n1;
-    let n2;
-    let resultado = n1**n2;
+function potencia (base, expoente) {
+    let result = 1;
+    for (let i = 0; i < expoente; i++) {
+      result *= base;
+    }
 
-    n1 = prompt("Digite o primeiro número: ");
+    n1 = prompt("Digite o primeiro número: base");
     n2 = prompt("Digite o segundo número:");
-
-    console.log(resultado);
+    
 }
+console.log(result);
+return result;
