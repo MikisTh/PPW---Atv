d28c1783281e4ce87ef95e998a901876ae1cc31b
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 08:14:32 -0300

    Update Ex09.js
    add

--- Atv 05/Ex09.js
+++ Atv 05/Ex09.js
@@ -1,6 +1,11 @@
 // Implemente uma função que receba um número como parâmetro e retorne sua representação binária. Utilize essa função para converter diferentes números em binário.
-function numeroParaBinario(num) {
-    return num.toString(2);
-  }
 
-console.log(numeroParaBinario(num));  
+function binario(n) {
+    let bin = ''
+    while (n > 0) {
+        bin += n % 2
+        n = Math.floor(n / 2)
+    }
+    return bin
+}
+
