7e81d060a645e142750b71c457202558a4112e6e
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 08:34:21 -0300

    Update Ex06.js
    Add

--- Atv 05/Ex06.js
+++ Atv 05/Ex06.js
@@ -1,17 +1,5 @@
 //Implemente uma função que inverta a ordem dos elementos em um array. Utilize essa função para inverter a ordem de diferentes conjuntos de elementos.
-function inverterArray(arr) {
-    return arr.reverse();
-  }
-
-let numeros = [1, 2, 3, 4, 5];
-let letras = ['a', 'b', 'c', 'd', 'e'];
-let conjuntos = [];
-
-
-numeros = inverterArray(numeros);
-letras = inverterArray(letras);
-conjuntos = inverterArray(conjuntos);
-
-console.log(numeros); 
-console.log(letras);
-console.log(conjuntos);
+function inverterArray(array) {
+    return array.reverse();
+}
+console.log(inverterArray([1, 2, 3, 5, 7, 9, 10, 12, 14, 16]));
