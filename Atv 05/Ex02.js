f4d8f6cf8b014c187fca0751e45c22e7e078ad32
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 08:25:51 -0300

    Update Ex02.js
    Add

--- Atv 05/Ex02.js
+++ Atv 05/Ex02.js
@@ -1,5 +1,11 @@
 // Desenvolva uma função que receba um número como parâmetro e verifique se ele é par ou ímpar. Retorne true se for par e false se for ímpar.
-function verificarParOuImpar(num) {
-    return num % 2 === 0;
+function parOuImpar(numero) {
+    if (numero % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
-  console.log(verificarParOuImpar(num)); 
+
+console.log(parOuImpar());
+
