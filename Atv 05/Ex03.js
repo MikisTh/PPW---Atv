705294236b604716a5ec5370525b0f3b24ebc662
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 08:24:14 -0300

    Update Ex03.js
    Add

--- Atv 05/Ex03.js
+++ Atv 05/Ex03.js
@@ -1,13 +1,10 @@
 //Implemente uma função que calcule a média aritmética de um array de números e retorne o resultado. Utilize essa função para calcular a média de diferentes conjuntos de números.
-function calcularMedia(numeros) {
-    const soma = numeros.reduce((acumulador, atual) => acumulador + atual, 0);
-    return soma / numeros.length;
-  }
-  
-const conjunto1 = [10, 20, 30, 40, 50];
-const media1 = calcularMedia(conjunto1);
-console.log(`A média do conjunto1 é: ${media1}`);
+function media(array) {
+    let soma = 0;
+    for (let i = 0; i < array.length; i++) {
+        soma += array[i];
+    }
+    return soma / array.length;
+}
 
-const conjunto2 = [5, 15, 25, 35, 45];
-const media2 = calcularMedia(conjunto2);
-console.log(`A média do conjunto2 é: ${media2}`);
+console.log(media([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
