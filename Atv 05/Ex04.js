95c5509c8d9252a5572614eb77cafc6d83af7c35
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 08:30:34 -0300

    Update Ex04.js
    Add

--- Atv 05/Ex04.js
+++ Atv 05/Ex04.js
@@ -1,6 +1,5 @@
 //Crie uma função que receba uma string como parâmetro e retorne a mesma string com todas as letras em caixa alta. Utilize essa função para converter diferentes strings.
-function converterParaMaiusculas(texto) {
-    return texto.toUpperCase();
-  }
-  console.log(converterParaMaiusculas(texto)); 
- 
+function caixaAlta(string) {
+    return string.toUpperCase();
+}
+console.log(caixaAlta("hello thiago mikis"));
