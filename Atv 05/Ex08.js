f20efcab8d52096f87b41a1f7a39ad62a65be346
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 08:39:53 -0300

    Update Ex08.js
    add

--- Atv 05/Ex08.js
+++ Atv 05/Ex08.js
@@ -1,6 +1,7 @@
 // Desenvolva uma função que verifique se uma palavra é um palíndromo (lê-se da mesma forma da esquerda para a direita e vice-versa). Retorne true se for um palíndromo e false se não for.
-function ehPalindromo(palavra) {
-    var palavraReversa = palavra.split('').reverse().join('');
-    return palavra === palavraReversa;
-  }
-console.log(ehPalindromo(palavra)); 
+function palindromo(palavra) {
+    return palavra === palavra.split('').reverse().join('');
+}
+
+console.log(palindromo('arara'));
+console.log(palindromo('tucano'));
