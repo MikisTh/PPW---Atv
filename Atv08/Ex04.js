7ecc984b51a3d69e41e9dbf77b0f024bf5905f76
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-11 20:14:28 -0300

    Update Ex04.js
    alter

--- Atv08/Ex04.js
+++ Atv08/Ex04.js
@@ -1,12 +1,12 @@
-//Os números Tribonacci são definidos pela seguinte recursão:
+//Os números fibonacci são definidos pela seguinte recursão:
 // Implemente um método recursivo que receba por parâmetro um número inteiro positivo
 // N e mostre e retorne o N-ésimo número da sequência de Tribonacci.
 
-function tribonacci(n) {
+function fibonacci(n) {
   if (n <= 2) {
     return 1;
   }
-  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+  return fibonacci(n - 1) + fibonacci(n - 2) + fibonacci(n - 3);
 }
 
-console.log(tribonacci(10));
+console.log(fibonacci(10));
